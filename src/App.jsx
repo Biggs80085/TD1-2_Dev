3bfeab905310f5809e72658f8edbcc32e31caebc
Biggs80085 <hamzasaoulajane9@gmail.com>
2024-02-01 16:17:39 +0100

    td3

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,26 @@ import React, { useState, useEffect } from 'react';
 const H1 = styled.h1`
   text_aline:center;
 `;
-const Div = styled.div`
-  margin: 10% 39%;
+const DivSeries = styled.div`
+  position: absolute;
+  margin-top:20px;
+  visibility:${props => (props.visibleSeries ? 'visible' : 'hidden')};
+`;
+const DivCount = styled.div`
+  position: absolute;
+  margin-top:20px;
   border:1px gray solid;
-  padding:20px 0px 20px 20px;
+  padding:20px;
   border-radius:30px;
+  visibility:${props => (props.visibleCount ? 'visible' : 'hidden')};
+`;
+const DivSeconds = styled.div`
+  position: absolute;
+  margin-top:20px;
+  border:1px gray solid;
+  padding:20px;
+  border-radius:30px;
+  visibility:${props => (props.visibleSeconds ? 'visible' : 'hidden')};
 `;
 const Affiche = styled.div`
   
@@ -17,13 +32,13 @@ const Affiche = styled.div`
 const Input = styled.input`
   border-radius: 30px;
   border: 2px gray solid;
-  padding-left
+  padding-left:5px;
 `;
 const Button = styled.button`
   background: transparent;
   border: 2px gray solid;
   border-radius: 30px;
-  width:50px;
+  padding:3px 15px;
   margin-left: 10px;
   &:hover{
     background:gray;
@@ -34,7 +49,32 @@ const Text = styled.p`
 `;
 function App() {
   const [seriesData, setSeriesData] = useState([]);
+  const [visibleSeries, setVisibleSeries] = useState(false);
+  const [visibleCount, setVisibleCount] = useState(false);
+  const [visibleSeconds, setVisibleSeconds] = useState(false);
 
+  const handleVisibleSeries = () => {
+    setVisibleSeries(!visibleSeries);
+    if(visibleSeries === false){
+      setVisibleCount(false)
+      setVisibleSeconds(false)
+    }
+  };
+  const handleVisibleCount = () => {
+    setVisibleCount(!visibleCount);
+    if(visibleCount === false){
+      setVisibleSeries(false)
+      setVisibleSeconds(false)
+    }
+    
+  };
+  const handleVisibleSeconds = () => {
+    setVisibleSeconds(!visibleSeconds);
+    if(visibleSeconds === false){
+      setVisibleCount(false)
+      setVisibleSeries(false)
+    }
+  };
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -88,7 +128,16 @@ function App() {
   //TD 3
   return (
   <>
-    <Div>
+    <Button onClick={handleVisibleSeries}>Series</Button>
+    <Button onClick={handleVisibleCount}>count</Button>
+    <Button onClick={handleVisibleSeconds}>Seconds</Button>
+    <DivSeries visibleSeries={visibleSeries}>
+      <H1>Liste des series</H1>
+        {seriesData.map((serie) => (
+            <Cadre serie={serie}/>
+        ))}
+    </DivSeries>
+    <DivCount visibleCount={visibleCount}>
       <Affiche>
         <Text>Nombre: {count}</Text>
         <Text>Incremente ou décremente par {value}</Text>
@@ -98,10 +147,10 @@ function App() {
         onChange={handleInputChange}/>
       <Button onClick={increment}>+</Button>
       <Button onClick={decrement}>-</Button>
-    </Div>
-    <Div>
+    </DivCount>
+    <DivSeconds visibleSeconds={visibleSeconds}>
       <Text>Le temps restant: {seconds > 0 ? seconds : 0}s</Text>
-    </Div>
+    </DivSeconds>
   </>
   )
 }
